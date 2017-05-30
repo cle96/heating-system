ffa25d47b9862ae93630d4e39beac66f66dfdd4a
Leahu Cristian <puiutzu96@yahoo.com>
2017-05-30 21:05:05 +0200

    Added references

---
+++ HeatingSystemWebApp/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HeatingSystemAdministration;
+
+namespace HeatingSystemWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+        public ActionResult Index(int? id = null)
+        {
+
+            using (var db = new Storage.StorageContext())
+            {
+                if (id != null)
+                {
+                    model.Customer = db.Customers.Find(id);
+                }
+                model.Matches = db.Matches.Where(m => m.MatchStart >= DateTime.Now).ToList();
+            }
+            return View(model);
+        }
+    }
+}
